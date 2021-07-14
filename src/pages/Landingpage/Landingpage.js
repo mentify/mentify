3358081e6210715ae5924f1372eebb7bf989d6f5
amalnj4858 <amalnj4858@gmail.com>
2021-07-15 00:44:33 +0530

    working on landing page middle part

--- src/pages/Landingpage/Landingpage.js
+++ src/pages/Landingpage/Landingpage.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Logo from "../../assets/Mentify-Logo.png";
 import Hello from "../../assets/Hello.png";
+import wave from "../../assets/wave.png";
 import styled from "styled-components";
 import Typewriter from "typewriter-effect";
 
 export const Landingpage = () => {
   const Landingpage = styled.div`
-    height: 90vh;
-    box-sizing: border-box;
-    margin: 0;
+    & {
+      height: 85vh;
+      box-sizing: border-box;
+      margin: 0;
+    }
     & .page1 {
       display: flex;
       justify-content: space-around;
@@ -23,10 +26,10 @@ export const Landingpage = () => {
       font-style: normal;
       font-weight: 500;
       font-size: 1.8rem;
-      line-height: 29px;
+      line-height: 1em;
     }
     & .helloImage {
-      width: 30%;
+      width: 30vw;
       height: auto;
     }
     & .logo {
@@ -39,6 +42,43 @@ export const Landingpage = () => {
       color: #64fcd9;
       font-size: 3rem;
       margin-top: 0.5em;
+    }
+    & .WaveImage {
+      width: 100%;
+      height: auto;
+      position: relative;
+      top: -8vh;
+      z-index: -2;
+    }
+    & .details {
+      padding-left: 3em;
+      padding-right: 3em;
+    }
+    & .details-heading {
+      font-family: Roboto;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 36px;
+      line-height: 42px;
+    }
+    & .details-description {
+      font-family: Rhodium Libre;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 24px;
+      line-height: 41px;
+    }
+    @media only screen and (max-width: 600px) {
+      & .page1 {
+        display: flex;
+        flex-direction: column;
+        padding-left: 3em;
+        padding-right: 3em;
+      }
+      & .helloImage {
+        width: 50vw;
+        height: auto;
+      }
     }
   `;
   return (
@@ -61,6 +101,23 @@ export const Landingpage = () => {
           </div>
         </div>
         <img src={Hello} alt="Welcome" className="helloImage" />
+      </div>
+      <img src={wave} alt="wave green" class="WaveImage" />
+      <div className="details">
+        <div className="details-heading">Why Mentify ?</div>
+        <div className="details-description">
+          Choosing a college right after 12th isnt an easy task , We help you
+          connect with students in those colleges you looking out for
+        </div>
+        <div className="details-description">
+          How to build my resume , Publish research papers ? We help you connect
+          with students who have walked the path you wish to walk
+        </div>
+        <div className="details-description">
+          Getting internship in while in college isnt an easy task , We help you
+          connect with students interning in various top firms you wish to
+          intern for
+        </div>
       </div>
     </Landingpage>
   );
