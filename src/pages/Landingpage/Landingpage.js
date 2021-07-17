913c8d0ad45c7790d789738502f4b0048eb7cfdc
amalnj4858 <amalnj4858@gmail.com>
2021-07-17 15:01:05 +0530

    making landing page responsive

--- src/pages/Landingpage/Landingpage.js
+++ src/pages/Landingpage/Landingpage.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Logo from "../../assets/Mentify-Logo.png";
 import Hello from "../../assets/Hello.png";
-import wave from "../../assets/wave.png";
+import topwave from "../../assets/TopWave.png";
+import bottomwave from "../../assets/BottomWave.png";
 import RoundImage from "../../assets/RoundImage.png";
+import search from "../../assets/search.png";
+import user from "../../assets/user-add.png";
+import laptop from "../../assets/laptop.png";
+import arrow from "../../assets/Arrow.png";
 import styled from "styled-components";
 import Typewriter from "typewriter-effect";
 
 export const Landingpage = () => {
   const Landingpage = styled.div`
     & {
-      height: 85vh;
+      display: flex;
+      flex-direction: column;
+      height: 100%;
       box-sizing: border-box;
       margin: 0;
     }
@@ -30,11 +37,14 @@ export const Landingpage = () => {
       line-height: 1em;
     }
     & .helloImage {
-      width: 30vw;
+      width: 35vw;
       height: auto;
     }
     & .logo {
       margin-top: 1em;
+    }
+    & .wave {
+      width: 100%;
     }
     & .ExamTitles {
       font-family: "Noto Sans JP", sans-serif;
@@ -44,30 +54,38 @@ export const Landingpage = () => {
       font-size: 3rem;
       margin-top: 0.5em;
     }
+    & .page2 {
+      position: relative;
+      top: -8vh;
+      display: flex;
+      flex-direction: column;
+    }
+
     & .details {
-      padding-left: 3em;
-      padding-right: 3em;
+      padding: 3em;
       background-size: cover;
-      height: 155vh;
+      height: fit-content;
       display: flex;
       flex-direction: column;
       justify-content: center;
-      position: relative;
-      top: -8vh;
+      background: #b5f7e7;
+      margin: 0;
     }
     & .details-heading {
       font-family: Roboto;
       font-style: normal;
       font-weight: bold;
-      font-size: 36px;
-      line-height: 42px;
+      font-size: 3rem;
+      line-height: 1em;
+      margin-bottom: 0.5em;
     }
     & .details-description {
       font-family: Rhodium Libre;
       font-style: normal;
       font-weight: normal;
-      font-size: 24px;
-      line-height: 41px;
+      font-size: 2rem;
+      line-height: 1.5em;
+      margin-bottom: 0.75em;
     }
     & .imageHolder {
       display: flex;
@@ -75,7 +93,90 @@ export const Landingpage = () => {
       position: relative;
       top: -40vh;
     }
-    @media only screen and (max-width: 600px) {
+    & .procedure {
+      justify-self: flex-start;
+      display: flex;
+      flex-direction: column;
+      padding-left: 3em;
+      padding-right: 3em;
+    }
+    & .procedureHeading {
+      font-family: Roboto;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 3rem;
+      text-align: left;
+      margin-bottom: 2rem;
+    }
+    & .steps {
+      margin-top: 0;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      text-align: center;
+      gap: 2em;
+    }
+    & .step {
+      width: 20%;
+    }
+    & .stepHeading {
+      font-family: Roboto;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 2rem;
+      line-height: 1em;
+      text-align: center;
+      margin-bottom: 1em;
+    }
+    & .stepContent {
+      font-family: Roboto;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 1.5rem;
+      line-height: 1.25em;
+      text-align: center;
+    }
+    & .arrow {
+      width: 15%;
+      height: 2rem;
+    }
+    & .emptyDiv {
+      margin-top: auto;
+      height: 30vh;
+      background: #b5f7e7;
+    }
+    @media (max-width: 1055px) {
+      & .page1 {
+        display: flex;
+        flex-direction: column;
+        padding-left: 3em;
+        padding-right: 3em;
+      }
+      & .steps {
+        flex-direction: column;
+      }
+      & .step {
+        width: 40%;
+      }
+      & .arrow {
+        transform: rotate(90deg);
+        transition-duration: 0.5s;
+        margin: 2em;
+      }
+      & .imageHolder {
+        top: -20vh;
+      }
+
+      & .procedureHeading {
+        text-align: center;
+      }
+
+      & .details-description {
+        font-size: 1.75rem;
+        line-height: 1.5em;
+      }
+    }
+    @media (max-width: 600px) {
       & .page1 {
         display: flex;
         flex-direction: column;
@@ -85,6 +186,9 @@ export const Landingpage = () => {
       & .helloImage {
         width: 50vw;
         height: auto;
+      }
+      & .imageHolder {
+        top: -20vh;
       }
     }
   `;
@@ -109,24 +213,57 @@ export const Landingpage = () => {
         </div>
         <img src={Hello} alt="Welcome" className="helloImage" />
       </div>
-      <div className="details" style={{ backgroundImage: `url(${wave})` }}>
-        <div className="details-heading">Why Mentify ?</div>
-        <div className="details-description">
-          Choosing a college right after 12th isnt an easy task , We help you
-          connect with students in those colleges you looking out for
+      <div className="page2">
+        <img src={topwave} alt="top" className="wave" />
+        <div className="details">
+          <div className="details-heading">Why Mentify ?</div>
+          <div className="details-description">
+            Choosing a college right after 12th isnt an easy task , We help you
+            connect with students in those colleges you looking out for
+          </div>
+          <div className="details-description">
+            How to build my resume , Publish research papers ? We help you
+            connect with students who have walked the path you wish to walk
+          </div>
+          <div className="details-description">
+            Getting internship in while in college isnt an easy task , We help
+            you connect with students interning in various top firms you wish to
+            intern for
+          </div>
         </div>
-        <div className="details-description">
-          How to build my resume , Publish research papers ? We help you connect
-          with students who have walked the path you wish to walk
-        </div>
-        <div className="details-description">
-          Getting internship in while in college isnt an easy task , We help you
-          connect with students interning in various top firms you wish to
-          intern for
-        </div>
+        <img src={bottomwave} alt="bottom" className="wave" />
       </div>
       <div className="imageHolder">
         <img src={RoundImage} alt="settings with lady" />
+      </div>
+      <div className="procedure">
+        <div className="procedureHeading">How do we work ?</div>
+        <div className="steps">
+          <div className="step">
+            <img src={search} alt="searchingg" />
+            <div className="stepHeading">Select a mentor</div>
+            <div className="stepContent">
+              Select a mentor based on the college/company you are looking for
+            </div>
+          </div>
+          <img src={arrow} alt="arrow" className="arrow" />
+          <div className="step">
+            <img src={laptop} alt="laptop" />
+            <div className="stepHeading">Book a Session</div>
+            <div className="stepContent">
+              Book a slot for a session with your mentor
+            </div>
+          </div>
+          <img src={arrow} alt="arrow" className="arrow" />
+          <div className="step">
+            <img src={user} alt="user" />
+            <div className="stepHeading">1-1 Mentoring Session</div>
+            <div className="stepContent">
+              Get a meeting invite on your email for a mentoring session with
+              your mentor and clarify all your queries.
+            </div>
+          </div>
+        </div>
       </div>
     </Landingpage>
   );
