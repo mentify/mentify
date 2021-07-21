51d24f7bae1561b49697515569b3d18111f94e28
Kuliscene <adhvaith9@gmail.com>
2021-07-21 21:16:49 +0530

    signin 1

--- src/pages/Signinpage/Signinpage.js
+++ src/pages/Signinpage/Signinpage.js
@@ -1,9 +1,61 @@
 import React from "react";
+import LogInMan from "../../assets/loginman.png";
+import './signin.css'
 
 export const Signinpage = () => {
-  return (
-    <div>
-      <h1>PWOLIKK</h1>
-    </div>
-  );
+	return (
+	<div className="limiter">
+		<div className= "login100-pic">
+			<img src={LogInMan}/>
+		</div>
+		<div className="container-login100">
+			<div className="wrap-login100">
+				<form className="login100-form validate-form">
+					<span className="login100-form-title">
+						Member Login
+					</span>
+
+					<div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input className="input100" type="text" name="email" placeholder="Email"/>
+						<span className="focus-input100"></span>
+						<span className="symbol-input100">
+							<i className="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div className="wrap-input100 validate-input" data-validate = "Password is required">
+						<input className="input100" type="password" name="pass" placeholder="Password"/>
+						<span className="focus-input100"></span>
+						<span className="symbol-input100">
+							<i className="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div className="container-login100-form-btn">
+						<button className="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+					<div className="text-center p-t-12">
+						<span className="txt1">
+							Forgot    
+						</span>
+						<br/>
+						<a className="txt2" href="#">
+							Username / Password?
+						</a>
+					</div>
+
+					<div className="text-center p-t-50">
+						<a className="txt2" href="#">
+							Create your Account
+							<i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	);
 };
