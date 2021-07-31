b5158ce320d00abd37cb2573c347ab42ecf79718
amalnj4858 <amalnj4858@gmail.com>
2021-07-31 22:51:12 +0530

    navbar formatting

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,17 +8,51 @@ export const Navbar = () => {
       height: 10vh;
       background: #b5f7e7;
       filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-      display:flex;
-      align-items:center;
-      gap:2em;
+      display: flex;
+      align-items: center;
+      gap: 2em;
     }
-    &
+    & .link {
+      text-decoration: none;
+      color: black;
+    }
+    & :last-child {
+      margin-right: 3em;
+    }
+    & :first-child {
+      margin-left: 3em;
+    }
+    & .auth {
+      margin-left: auto;
+    }
+    & .signup {
+      padding: 0.4em;
+      border: 2px black solid;
+      border-radius: 3em;
+      transition-duration: 0.5s;
+    }
+    & .signup:hover {
+      background: black;
+      color: #b5f7e7;
+    }
   `;
   return (
     <Nav className="Navbar">
-      <Link to="/"> Home </Link>
-      <Link to="/signin"> Sign In </Link>
-      <Link to="/register"> Register </Link>
+      <Link to="/" className="link">
+        Home
+      </Link>
+      <Link to="/mentors" className="link">
+        Mentors
+      </Link>
+      <Link to="/" className="link">
+        Contact Us
+      </Link>
+      <Link to="/signin" className="link auth">
+        Sign In
+      </Link>
+      <Link to="/register" className="link signup">
+        Sign Up
+      </Link>
     </Nav>
   );
 };
