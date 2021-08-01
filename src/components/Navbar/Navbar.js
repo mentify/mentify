66d9764abd5ecafec926b416de7e5bb1f4dd5ca2
amalnj4858 <amalnj4858@gmail.com>
2021-08-01 14:48:28 +0530

    added dropdown list

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,36 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import dropdown from "../../assets/dropdown-arrow.png";
 
 export const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const onDropDownClick = () => {
+    setIsVisible(!isVisible);
+  };
   const Nav = styled.div`
     & {
       height: 10vh;
+      min-height: 50px;
       background: #b5f7e7;
       filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
       display: flex;
       align-items: center;
       gap: 2em;
+      width: 100%;
+      box-sizing: border-box;
+      max-width: 100vw;
+      z-index: 2;
+    }
+    & .line {
+      border: 0;
+      border-top: 2px solid #eee;
+    }
+    & .drop {
+      padding-left: 1em;
     }
     & .link {
       text-decoration: none;
       color: black;
       transition-duration: 0.3s;
     }
-    & :last-child {
-      margin-right: 3em;
-    }
-    & :first-child {
+    & .home {
       margin-left: 3em;
     }
     & .auth {
       margin-left: auto;
     }
     & .signup {
+      margin-right: 3em;
       padding: 0.4em;
       border: 2px black solid;
       border-radius: 3em;
       transition-duration: 0.5s;
+    }
+    & .dropdown-img {
+      height: 3em;
+      cursor: pointer;
+      display: none;
+    }
+    & .dropdown-list {
+      position: absolute;
+      top: 10vh;
+      right: 0;
+      width: 30vh;
+      display: flex;
+      flex-direction: column;
+      padding-top: 1em;
+      padding-bottom: 1em;
+      gap: 1em;
+      background: white;
+      z-index: 3;
+      border: 1px gray solid;
+      display: none;
     }
     & .link:hover {
       color: gray;
@@ -39,16 +74,32 @@ export const Navbar = () => {
       background: black;
       color: #b5f7e7;
     }
+    @media (max-width: 768px) {
+      & .contact {
+        display: none;
+      }
+      & .auth {
+        display: none;
+      }
+      & .signup {
+        display: none;
+      }
+      & .dropdown-img {
+        display: flex;
+        margin-left: auto;
+        margin-right: 3em;
+      }
+    }
   `;
   return (
     <Nav className="Navbar">
-      <Link to="/" className="link">
+      <Link to="/" className="link home">
         Home
       </Link>
       <Link to="/mentors" className="link">
         Mentors
       </Link>
-      <Link to="/" className="link">
+      <Link to="/contact" className="link contact">
         Contact Us
       </Link>
       <Link to="/signin" className="link auth">
@@ -57,6 +108,27 @@ export const Navbar = () => {
       <Link to="/register" className="link signup">
         Sign Up
       </Link>
+      <img src={dropdown} className="dropdown-img" onClick={onDropDownClick} />
+      <div
+        className="dropdown-list"
+        style={{ display: isVisible ? "flex" : "none" }}
+      >
+        <Link to="/signin" className="link drop" onClick={onDropDownClick}>
+          Sign In
+        </Link>
+        <hr className="line" />
+        <Link to="/signup" className="link drop" onClick={onDropDownClick}>
+          Sign Up
+        </Link>
+        <hr className="line" />
+        <Link to="/signin" className="link drop" onClick={onDropDownClick}>
+          Log out
+        </Link>
+        <hr className="line" />
+        <Link to="/contact" className="link drop" onClick={onDropDownClick}>
+          Contact Us
+        </Link>
+      </div>
     </Nav>
   );
 };
--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -3,450 +3,473 @@ import styled from "styled-components";
 import googleLogo from "../../assets/googlelogo.svg";
 
 const SignInFormStyled = styled.div`
-	& a {
-		font-family: "Poppins", sans-serif;
-		font-weight: 400;
-		font-size: 1rem;
-		line-height: 1.7;
-		color: #666666;
-		margin: 0px;
-		transition: all 0.4s;
-	}
+  & a {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.7;
+    color: #666666;
+    margin: 0px;
+    transition: all 0.4s;
+  }
 
-	& a:focus {
-		outline: none !important;
-	}
+  & a:focus {
+    outline: none !important;
+  }
 
-	& a:hover {
-		text-decoration: none;
-		color: #57b846;
-	}
+  & a:hover {
+    text-decoration: none;
+    color: #57b846;
+  }
 
-	& p {
-		font-family: "Poppins", sans-serif;
-		font-weight: 400;
-		font-size: 1rem;
-		line-height: 1.7;
-		color: #666666;
-		margin: 0px;
-	}
+  & p {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.7;
+    color: #666666;
+    margin: 0px;
+  }
 
-	& input {
-		outline: none;
-		border: none;
-	}
+  & input {
+    outline: none;
+    border: none;
+  }
 
-	& input::-webkit-input-placeholder {
-		color: #999999;
-	}
-	& input:-moz-placeholder {
-		color: #999999;
-	}
-	& input::-moz-placeholder {
-		color: #999999;
-	}
-	& input:-ms-input-placeholder {
-		color: #999999;
-	}
+  & input::-webkit-input-placeholder {
+    color: #999999;
+  }
+  & input:-moz-placeholder {
+    color: #999999;
+  }
+  & input::-moz-placeholder {
+    color: #999999;
+  }
+  & input:-ms-input-placeholder {
+    color: #999999;
+  }
 
-	& button {
-		outline: none !important;
-		border: none;
-		background: transparent;
-	}
+  & button {
+    outline: none !important;
+    border: none;
+    background: transparent;
+  }
 
-	& button:hover {
-		cursor: pointer;
-	}
+  & button:hover {
+    cursor: pointer;
+  }
 
-	& iframe {
-		border: none !important;
-	}
+  & iframe {
+    border: none !important;
+  }
 
-	& .txt1 {
-		font-family: "Poppins", sans-serif;
-		font-weight: 400;
-		font-size: 1rem;
-		line-height: 1.5;
-		color: #999999;
-	}
+  & .txt1 {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.5;
+    color: #999999;
+  }
 
-	& .txt2 {
-		font-family: "Poppins", sans-serif;
-		font-weight: 400;
-		font-size: 1rem;
-		line-height: 1.5;
-		color: #666666;
-	}
+  & .txt2 {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.5;
+    color: #666666;
+  }
 
-	& {
-		width: 30%;
-		height: 90vh;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-		align-items: center;
-		box-sizing: border-box;
-		background: white;
-	}
+  & {
+    width: 30%;
+    height: 90vh;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    background: white;
+  }
 
-	& .wrap-login100 {
-		width: 95%;
-		border: 2px solid black;
-		background: white;
-		border-radius: 10px;
-		overflow: hidden;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-		align-items: center;
-		padding: 2em 2em 2em 2em;
-		box-shadow: 10px 10px 5px grey;
-	}
+  & .wrap-login100 {
+    width: 95%;
+    border: 2px solid black;
+    background: white;
+    border-radius: 10px;
+    overflow: hidden;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    padding: 2em 2em 2em 2em;
+    box-shadow: 10px 10px 5px grey;
+  }
 
-	& .p-t-12 {
-		padding-top: 1em;
-	}
-	& .p-t-35 {
-		padding-top: 35px;
-	}
+  & .p-t-12 {
+    padding-top: 1em;
+  }
+  & .p-t-35 {
+    padding-top: 35px;
+  }
 
-	& .login100-form {
-		width: 90%;
-	}
+  & .login100-form {
+    width: 90%;
+  }
 
-	& .text-center {
-		text-align: center;
-	}
+  & .text-center {
+    text-align: center;
+  }
 
-	& .login100-form-title {
-		font-family: ABeeZee;
-		font-size: 2rem;
-		color: black;
-		line-height: 1.2;
-		text-align: center;
-		width: 100%;
-		display: block;
-		padding-bottom: 54px;
-	}
+  & .login100-form-title {
+    font-family: ABeeZee;
+    font-size: 2rem;
+    color: black;
+    line-height: 1.2;
+    text-align: center;
+    width: 100%;
+    display: block;
+    padding-bottom: 54px;
+  }
 
-	& .wrap-input100 {
-		position: relative;
-		width: 100%;
-		z-index: 1;
-		margin-bottom: 10px;
-	}
+  & .wrap-input100 {
+    position: relative;
+    width: 100%;
+    z-index: 1;
+    margin-bottom: 10px;
+  }
 
-	& .input100 {
-		font-family: "Poppins", sans-serif;
-		font-weight: 500;
-		font-size: 0.8rem;
-		line-height: 1.5;
-		color: #666666;
-		display: block;
-		width: 100%;
-		background: #e6e6e6;
-		height: 50px;
-		border-radius: 25px;
-		padding: 0 4em 0 4em;
-	}
+  & .input100 {
+    font-family: "Poppins", sans-serif;
+    font-weight: 500;
+    font-size: 0.8rem;
+    line-height: 1.5;
+    color: #666666;
+    display: block;
+    width: 100%;
+    background: #e6e6e6;
+    height: 50px;
+    border-radius: 25px;
+    padding: 0 4em 0 4em;
+  }
 
-	& .focus-input100 {
-		display: block;
-		position: absolute;
-		border-radius: 25px;
-		bottom: 0;
-		left: 0;
-		z-index: -1;
-		width: 100%;
-		height: 100%;
-		box-shadow: 0px 0px 0px 0px;
-		color: rgba(87, 184, 70, 0.8);
-	}
+  & .focus-input100 {
+    display: block;
+    position: absolute;
+    border-radius: 25px;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    box-shadow: 0px 0px 0px 0px;
+    color: rgba(87, 184, 70, 0.8);
+  }
 
-	& .input100:focus + .focus-input100 {
-		animation: anim-shadow 0.5s ease-in-out forwards;
-	}
+  & .input100:focus + .focus-input100 {
+    animation: anim-shadow 0.5s ease-in-out forwards;
+  }
 
-	& .symbol-input100 {
-  font-size: 15px;
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: flex;
-  align-items: center;
-  position: absolute;
-  border-radius: 25px;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  padding-left: 35px;
-  pointer-events: none;
-  color: #666666;
-  -webkit-transition: all 0.4s;
-  -o-transition: all 0.4s;
-  -moz-transition: all 0.4s;
-  transition: all 0.4s;
-}
+  & .symbol-input100 {
+    font-size: 15px;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: flex;
+    align-items: center;
+    position: absolute;
+    border-radius: 25px;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding-left: 35px;
+    pointer-events: none;
+    color: #666666;
+    -webkit-transition: all 0.4s;
+    -o-transition: all 0.4s;
+    -moz-transition: all 0.4s;
+    transition: all 0.4s;
+  }
 
-	@-webkit-keyframes anim-shadow {
-		to {
-			box-shadow: 0px 0px 70px 25px;
-			opacity: 0;
-		}
-	}
+  @-webkit-keyframes anim-shadow {
+    to {
+      box-shadow: 0px 0px 70px 25px;
+      opacity: 0;
+    }
+  }
 
-	@keyframes anim-shadow {
-		to {
-			box-shadow: 0px 0px 70px 25px;
-			opacity: 0;
-		}
-	}
+  @keyframes anim-shadow {
+    to {
+      box-shadow: 0px 0px 70px 25px;
+      opacity: 0;
+    }
+  }
 
-	& .input100:focus + .focus-input100 + .symbol-input100 {
-  color: #57b846;
-  padding-left: 28px;
-}
+  & .input100:focus + .focus-input100 + .symbol-input100 {
+    color: #57b846;
+    padding-left: 28px;
+  }
 
-	& .container-login100-form-btn {
-		width: 100%;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-		padding-top: 20px;
-	}
+  & .container-login100-form-btn {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding-top: 20px;
+  }
 
-	& .login100-form-btn {
-		font-family: "Montserrat", sans-serif;
-		font-size: 15px;
-		line-height: 1.5;
-		color: black;
-		width: 100%;
-		height: 42px;
-		border-radius: 25px;
-		background: #B5F7E7;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		padding: 0 25px;
-		transition: all 0.4s;
-	}
+  & .login100-form-btn {
+    font-family: "Montserrat", sans-serif;
+    font-size: 15px;
+    line-height: 1.5;
+    color: black;
+    width: 100%;
+    height: 42px;
+    border-radius: 25px;
+    background: #b5f7e7;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 25px;
+    transition: all 0.4s;
+  }
 
-	& .login100-form-btn:hover {
-		background: #FFB61D;
-	}
+  & .login100-form-btn:hover {
+    background: #ffb61d;
+  }
 
-	& .orline{
-		 width: 100%; 
-   text-align: center; 
-   border-bottom: 1px solid #000; 
-   line-height: 0.1em;
-   margin: 10px 0 20px; 
-	}
- 
- & .orline span{
-    background:#fff; 
-    padding:0 10px;
- }
-	& .google-btn {
-		width: auto;
-		margin-top:0.6em;
-		height: 42px;
-		background-color: white;
-		border-radius: 25px;
-		box-shadow: 0 3px 4px 0 rgba(0,0,0,.25);
-	& .google-icon-wrapper {
-		position: absolute;
-		margin-top: 1px;
-		margin-left: 1px;
-		width: 40px;
-		height: 40px;
-		border-radius: 25px;
-		background-color: white;
-	}
-	& .google-icon {
-		position: absolute;
-		margin-top: 11px;
-		margin-left: 11px;
-		width: 18px;
-		height: 18px;
-	}
-	& .btn-text {
-		padding-top:0.6em;
-		text-align:center;
-		margin: 11px 11px 0 0;
-		color: #4285f4;
-		font-size: 14px;
-		letter-spacing: 0.2px;
-		font-family: "Roboto";
-	}
+  & .orline {
+    width: 100%;
+    text-align: center;
+    border-bottom: 1px solid #000;
+    line-height: 0.1em;
+    margin: 10px 0 20px;
+  }
 
-	&:hover {
-		box-shadow: 0 0 6px #4285f4;
-	}
+  & .orline span {
+    background: #fff;
+    padding: 0 10px;
+  }
+  & .google-btn {
+    width: auto;
+    margin-top: 0.6em;
+    height: 42px;
+    background-color: white;
+    border-radius: 25px;
+    box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.25);
+    & .google-icon-wrapper {
+      position: absolute;
+      margin-top: 1px;
+      margin-left: 1px;
+      width: 40px;
+      height: 40px;
+      border-radius: 25px;
+      background-color: white;
+    }
+    & .google-icon {
+      position: absolute;
+      margin-top: 11px;
+      margin-left: 11px;
+      width: 18px;
+      height: 18px;
+    }
+    & .btn-text {
+      padding-top: 0.6em;
+      text-align: center;
+      margin: 11px 11px 0 0;
+      color: #4285f4;
+      font-size: 14px;
+      letter-spacing: 0.2px;
+      font-family: "Roboto";
+    }
 
-	&:active {
-		background: #1669F2;
-}
-}
-	@media (max-width: 1290px) {
-		& {
-			width: 35%;
-		}
-	}
+    &:hover {
+      box-shadow: 0 0 6px #4285f4;
+    }
 
-	@media (max-width: 1290px) {
-		& {
-			width: 35%;
-		}
-	}
+    &:active {
+      background: #1669f2;
+    }
+  }
+  @media (max-width: 1290px) {
+    & {
+      width: 35%;
+    }
+  }
 
-	@media (max-width: 992px) {
-		.login100-pic {
-			width: 45%;
-		}
-		& {
-			width: 40%;
-		}
-	}
+  @media (max-width: 1290px) {
+    & {
+      width: 35%;
+    }
+  }
 
-	@media (max-width: 768px) {
-		.login100-pic {
-			display: none;
-		}
+  @media (max-width: 992px) {
+    .login100-pic {
+      width: 45%;
+    }
+    & {
+      width: 40%;
+    }
+  }
 
-		& {
-			width: 55vw;
-		}
+  @media (max-width: 768px) {
+    .login100-pic {
+      display: none;
+    }
 
-		.login100-form {
-			width: 100%;
-		}
-	}
+    & {
+      width: 55vw;
+    }
 
-	@media (max-width: 576px) {
-		& {
-			width: 80vw;
-		}
-	}
-	
+    .login100-form {
+      width: 100%;
+    }
+  }
+
+  @media (max-width: 576px) {
+    & {
+      width: 80vw;
+    }
+  }
 `;
 
 export const SignInForm = ({ heading }) => {
-	return (
-		<SignInFormStyled className="container-login100">
-			<div className="wrap-login100">
-				<form className="login100-form">
-					<span className="login100-form-title">{heading==="Sign In"? <div><b>Welcome back</b></div>: <div><b>Welcome</b></div>}</span>
-					{ heading=== 'Sign Up'?
-					<div>
-						<div className="wrap-input100 ">
-							<input
-								className="input100"
-								type="text"
-								name="name"
-								placeholder="  Name"
-							/>
-							<span className="focus-input100"></span>
-							<span className="symbol-input100"><i class="fa fa-user" aria-hidden="true"></i></span>
-						</div>
+  return (
+    <SignInFormStyled className="container-login100">
+      <div className="wrap-login100">
+        <form className="login100-form">
+          <span className="login100-form-title">
+            {heading === "Sign In" ? (
+              <div>
+                <b>Welcome back</b>
+              </div>
+            ) : (
+              <div>
+                <b>Welcome</b>
+              </div>
+            )}
+          </span>
+          {heading === "Sign Up" ? (
+            <div>
+              <div className="wrap-input100 ">
+                <input
+                  className="input100"
+                  type="text"
+                  name="name"
+                  placeholder="  Name"
+                />
+                <span className="focus-input100"></span>
+                <span className="symbol-input100">
+                  <i class="fa fa-user" aria-hidden="true"></i>
+                </span>
+              </div>
 
-						<div className="wrap-input100 ">
-							<input
-								className="input100"
-								type="tel"
-								name="phone_no"
-								placeholder="  Mobile Number"
-								maxlength="10"
-							/>
-							<span className="focus-input100"></span>
-							<span className="symbol-input100"><i class="fa fa-phone-square" aria-hidden="true"></i></span>
-						</div>
-					</div>
-					:
-					<div>
-					</div>
-					}
+              <div className="wrap-input100 ">
+                <input
+                  className="input100"
+                  type="tel"
+                  name="phone_no"
+                  placeholder="  Mobile Number"
+                  maxlength="10"
+                />
+                <span className="focus-input100"></span>
+                <span className="symbol-input100">
+                  <i class="fa fa-phone-square" aria-hidden="true"></i>
+                </span>
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
 
-					<div className="wrap-input100 ">
-						<input
-							className="input100"
-							type="text"
-							name="email"
-							placeholder="  Email"
-						/>
-						<span className="focus-input100"></span>
-						<span className="symbol-input100"><i class="fa fa-envelope fa-1x" aria-hidden="true"></i></span>
-					</div>
+          <div className="wrap-input100 ">
+            <input
+              className="input100"
+              type="text"
+              name="email"
+              placeholder="  Email"
+            />
+            <span className="focus-input100"></span>
+            <span className="symbol-input100">
+              <i class="fa fa-envelope fa-1x" aria-hidden="true"></i>
+            </span>
+          </div>
 
-					<div className="wrap-input100">
-						<input
-							className="input100"
-							type="password"
-							name="pass"
-							placeholder="  Password"
-						/>
-						<span className="focus-input100"></span>
-						<span className="symbol-input100"><i class="fa fa-key" aria-hidden="true"></i></span>
-					</div>
+          <div className="wrap-input100">
+            <input
+              className="input100"
+              type="password"
+              name="pass"
+              placeholder="  Password"
+            />
+            <span className="focus-input100"></span>
+            <span className="symbol-input100">
+              <i class="fa fa-key" aria-hidden="true"></i>
+            </span>
+          </div>
 
-					{heading==="Sign Up"?
-					<div className="text-center p-t-12 ">
-						<input type="checkbox" id="tnc" name="tnc" value="agree"/>	
-							<label for="tnc"> 
-							<span className="txt1"> I agree to terms and conditions
-							</span>
-							</label>
-					</div>
-					:<div></div>}
-					<div className="container-login100-form-btn">
-						<button className="login100-form-btn"><b>{heading}</b></button>
-					</div>
-					<div className=" orline text-center p-t-12 ">
-						<span>OR</span>
-					</div>
-					<div class="google-btn">
-						<div class="google-icon-wrapper">
-								<img class="google-icon" src={googleLogo}/>
-						</div>
-						{heading==="Sign In"?
-							<div>
-								<p class="btn-text"><b>Sign in with google</b></p>
-							</div>
-							:<div>
-								<p class="btn-text"><b>Sign up with google</b></p>
-							</div>
-						}
-					</div>
+          {heading === "Sign Up" ? (
+            <div className="text-center p-t-12 ">
+              <input type="checkbox" id="tnc" name="tnc" value="agree" />
+              <label for="tnc">
+                <span className="txt1"> I agree to terms and conditions</span>
+              </label>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <div className="container-login100-form-btn">
+            <button className="login100-form-btn">
+              <b>{heading}</b>
+            </button>
+          </div>
+          <div className=" orline text-center p-t-12 ">
+            <span>OR</span>
+          </div>
+          <div class="google-btn">
+            <div class="google-icon-wrapper">
+              <img class="google-icon" src={googleLogo} alt="google" />
+            </div>
+            {heading === "Sign In" ? (
+              <div>
+                <p class="btn-text">
+                  <b>Sign in with google</b>
+                </p>
+              </div>
+            ) : (
+              <div>
+                <p class="btn-text">
+                  <b>Sign up with google</b>
+                </p>
+              </div>
+            )}
+          </div>
 
-					{ heading==="Sign In"?
-						<div>
-							<div className="text-center p-t-12 ">
-								<span className="txt1">Forgot</span>
-								<br />
-								<a className="txt2" href="#">
-									Username / Password?
-								</a>
-							</div>
+          {heading === "Sign In" ? (
+            <div>
+              <div className="text-center p-t-12 ">
+                <span className="txt1">Forgot</span>
+                <br />
+                <a className="txt2" href="#">
+                  Username / Password?
+                </a>
+              </div>
 
-							<div className="text-center p-t-35">
-								<a className="txt2" href="#">
-									Create your Account
-								</a>
-							</div>
-						</div>
-					:
-					<div className="text-center p-t-35">
-						<a className="txt2" href="#">
-								Already a user? Sign in
-						</a>
-					</div>
-					}
-					
-				</form>
-			</div>
-		</SignInFormStyled>
-	);
+              <div className="text-center p-t-35">
+                <a className="txt2" href="#">
+                  Create your Account
+                </a>
+              </div>
+            </div>
+          ) : (
+            <div className="text-center p-t-35">
+              <a className="txt2" href="#">
+                Already a user? Sign in
+              </a>
+            </div>
+          )}
+        </form>
+      </div>
+    </SignInFormStyled>
+  );
 };
