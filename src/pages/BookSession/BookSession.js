32a9914d4d5e207b959f8ef75a0e9b61f96be856
Kuliscene <adhvaith9@gmail.com>
2021-09-24 22:10:45 +0530

    hi

--- src/pages/BookSession/BookSession.js
+++ src/pages/BookSession/BookSession.js
@@ -126,10 +126,11 @@ const BookSessionStyled = styled.div`
     }
     & .calendar{
         width:30em;
-        height:14em;
+        height:fit-content;
         border:0.1em solid black; 
         background-color:#F8F8FF;
         display:flex;
+        padding:1em;
         font-size:1em;
         font-weight:800;
         justify-content:center;
@@ -183,37 +184,122 @@ const BookSessionStyled = styled.div`
         cursor: pointer;  
         border: 0.1em solid black;
     }
+    & .timeslot{
+        height:20em;
+        width:fit-content;
+        border:0.1em solid black; 
+        display:flex;
+        padding:1em;    
+        flex-direction:column;
+    }
+
+    & .timeslotbtn{
+        height:4em;
+        margin-bottom:1em;
+        border-radius:0.7em;
+        border: 0.1em solid black
+
+    }
     `;
 
     export class BookSession extends React.Component {
-        disabledDates = [
-          new Date(2021, 8, 23),
-          new Date(2021, 8, 22),
-        ];
+        preferredDays = [2,4,6];
+        preferredTimes=[7,8,15,16,22,23]
+        mentorEmailId="adhvaithkul@gmail.com"
+        preferredSlots = new Map([[2, [7,9,17,18]], [4 ,[15,16]] ,[6,[22,23]]]);
 
+        gapi=window.gapi
+        CLIENT_ID="487444471330-c1io6lpljdgh3sq5rrs2qurp80v2bjn2.apps.googleusercontent.com"
+        API_KEY="AIzaSyAYLG6TgLw2GSp4r83gc8Dey4ADEagOYF4"
+        DISCOVERY_DOCS=["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
+        SCOPES = "https://www.googleapis.com/auth/calendar"
+        
         constructor() {
             super()
             this.state = {
                 date:'',
-                today:new Date()
+                slots:""
             }
         }
 
         onchange=(date)=>{
             this.setState({date:date })
+            /*this.state.slots.length=0
+            let a=date.getDay()
+            for(let [key,value] of this.preferredSlots){
+                if(key===a){
+                    this.state.slots.push(value)
+                }
+            }*/
+            let a=date.getDay()
+            let b=this.preferredSlots.get(a)
+            this.setState({slots:b})
         }
 
         isSameDay=(a, b)=> {
-          return differenceInCalendarDays(a, b) === 0;
+          return b.getDay()=== a;
         }
 
         tileDisabled=({ date, view })=> {
-          // Disable tiles in month view only
           if (view === 'month') {
-            // Check if a date React-Calendar wants to check is on the list of disabled dates
-            return this.disabledDates.find(dDate => this.isSameDay(dDate, date));
+            for(let key of this.preferredSlots.keys()){
+                if(this.isSameDay(key,date)){
+                    return(false)
+                }
+            }
+            return(true)
           }
         }
+
+        createEvent=()=>{
+            this.gapi.load('client:auth2', () => {
+                this.gapi.client.init({
+                    apiKey:this.API_KEY,
+                    clientId:this.CLIENT_ID,
+                    discoveryDocs:this.DISCOVERY_DOCS,
+                    scope:this.SCOPES
+                })
+                this.gapi.client.load('calendar','v3',()=>console.log("loaded"))
+                this.gapi.auth2.getAuthInstance().signIn()
+                .then(()=>{
+                    var event = {
+                              'summary': 'Mentorship',
+                              'description': 'Really great refreshments',
+                              'start': {
+                                'dateTime': '2021-09-24T09:00:00-07:00',
+                                'timeZone': 'America/Los_Angeles'
+                              },
+                              'end': {
+                                'dateTime': '2021-09-24T17:00:00-07:00',
+                                'timeZone': 'India/Kolkata'
+                              },
+                              'recurrence': [
+                                'RRULE:FREQ=DAILY;COUNT=2'
+                              ],
+                              'attendees': [
+                                {'email': this.mentorEmailId},
+                              ],
+                              'reminders': {
+                                'useDefault': false,
+                                'overrides': [
+                                  {'method': 'email', 'minutes': 24 * 60},
+                                  {'method': 'popup', 'minutes': 10}
+                                ]
+                              }
+                            }
+                        var request = this.gapi.client.calendar.events.insert({
+                                  'calendarId': 'primary',
+                                  'resource': event,
+                                })
+
+                                request.execute(event => {
+                                  console.log(event)
+                                  window.open(event.htmlLink)
+                                })
+                })  
+            })
+        }
+
 
         render(){return (
                 <BookSessionStyled className="BookSessionStyled">
@@ -238,24 +324,28 @@ const BookSessionStyled = styled.div`
                                 <div className="scores"><span className="test">BITSAT</span><span className="yellow1">277</span></div>
                             </div>
                         </div>
+                        <div className="collegeadmits">
+                            <div className="collegeadmitsheading">College Admits</div>
+                            <div className="colleges">IIT BHU , BITS GOA , VIT ,SRM, MIT</div>
+                        </div>
+                    </div>
+                    <div className="bottompart">
                         <div className="calendar">
                             <Calendar
                                 onChange={this.onchange}
                                 value={this.state.date}
                                 tileDisabled={this.tileDisabled}
-
+                                onClickDay={this.onClickDay}
                             />
-                            {console.log(this.state.date)}
-                            {console.log(this.disabledDates)}
                         </div>
-                    </div>
-                    <div className="bottompart">
-                        <div className="collegeadmits">
-                            <div className="collegeadmitsheading">College Admits</div>
-                            <div className="colleges">IIT BHU , BITS GOA , VIT ,SRM, MIT</div>
-                        </div>
-                        <div className="bookbtn">
-                            <button>Book a Session!</button>
+                        {}
+                        <div className="timeslot">
+                            {this.state.date?<div>Check Availability of Time Slot</div>:null}
+                            
+                            
+                            <div className="bookbtn">
+                                <button onClick={this.createEvent}>{this.state.date?<p>Book Session</p>:<p> Book Session</p>}</button>
+                            </div>
                         </div>
                     </div>
                     <Footer/>
